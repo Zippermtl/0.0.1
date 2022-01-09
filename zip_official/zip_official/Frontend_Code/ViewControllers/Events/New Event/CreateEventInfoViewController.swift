c49c16b2f2987a732ecf607423225ea4ebf61001
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-01-09 14:31:51 -0500

    UI Update

---
+++ zip_official/zip_official/Frontend_Code/ViewControllers/Events/New Event/CreateEventInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateEventInfoViewController.swift
+//  zip_official
+//
+//  Created by Yianni Zavaliagkos on 1/7/22.
+//
+
+import UIKit
+
+class CreateEventInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
