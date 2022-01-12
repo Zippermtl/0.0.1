71035d081ce167b931609b5786a91925c040fc86
Gabe <gabe2002denton@gmail.com>
2022-01-11 20:40:39 -0600

    spacing

--- zip_official/zip_official/Frontend_Code/ViewControllers/Map/Profile/ProfileViewController.swift
+++ zip_official/zip_official/Frontend_Code/ViewControllers/Map/Profile/ProfileViewController.swift
@@ -253,7 +253,6 @@ class ProfileViewController: UIViewController {
         
         //removes the text section of the back button from pushed VCs
         navigationItem.backBarButtonItem = BackBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        
         let dismissButton = UIButton(type: .system)
         dismissButton.setImage(UIImage(systemName: "chevron.left")?.withRenderingMode(.alwaysOriginal).withTintColor(.white), for: .normal)
         dismissButton.frame = CGRect(x: 0, y: 0, width: 1, height: 34)
