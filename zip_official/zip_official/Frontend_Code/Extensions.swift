154ee367be5b9b5bfc7ad86678fa4de63b2f1abb
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-01-18 17:09:18 -0500

    added extensions

--- zip_official/zip_official/Frontend_Code/Extensions.swift
+++ zip_official/zip_official/Frontend_Code/Extensions.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 import DropDown
 import RSKImageCropper
-
+import CoreLocation
 
 extension UIScrollView {
     func updateContentView(_ buffer: CGFloat = 0) {
@@ -45,4 +45,72 @@ extension Date {
         return lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate
     }
 
+}
+
+
+//creates frame with wrapped text to see what the height will be
+extension String {
+    func heightForWrap(width: CGFloat) -> CGFloat{
+        let tempLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        tempLabel.numberOfLines = 0
+        tempLabel.lineBreakMode = .byWordWrapping
+        tempLabel.font = .zipBody
+        tempLabel.text = self
+        tempLabel.sizeToFit()
+        return tempLabel.frame.height
+    }
+}
+
+
+extension UILabel {
+    func heightForWrap(width: CGFloat) -> CGFloat{
+        let tempLabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        tempLabel.numberOfLines = 0
+        tempLabel.lineBreakMode = .byWordWrapping
+        tempLabel.font = .zipBody
+        tempLabel.text = text
+        tempLabel.sizeToFit()
+        return tempLabel.frame.height
+    }
+}
+
+
+public func getDistanceLabel(user: User) -> String {
+    guard let coordinates = UserDefaults.standard.object(forKey: "userLoc") as? [Double] else {
+        return ""
+    }
+    
+    let userLoc = CLLocation(latitude: coordinates[0], longitude: coordinates[1])
+    var distance = Double(round(10*(userLoc.distance(from: user.location))/1000))/10
+
+    var unit = "km"
+    if NSLocale.current.regionCode == "US" {
+        distance = round(10*distance/1.6)/10
+        unit = "miles"
+    }
+    
+    if distance > 10 {
+        let intDistance = Int(distance)
+        if distance <= 1 {
+            if unit == "miles" {
+                unit = "mile"
+            }
+            return "<1 \(unit)"
+        } else if distance >= 500 {
+            return ">500 \(unit)"
+        } else {
+            return String(intDistance) + " \(unit)"
+        }
+    } else {
+        if distance <= 1 {
+            if unit == "miles" {
+                unit = "mile"
+            }
+            return "<1 \(unit)"
+        } else if distance >= 500 {
+            return ">500 \(unit)"
+        } else {
+            return String(distance) + " \(unit)"
+        }
+    }
 }
