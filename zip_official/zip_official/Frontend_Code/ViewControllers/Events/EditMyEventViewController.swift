9febcc4efb5588d9e9e3d71913986281c5f31d80
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-03-02 18:04:50 -0500

    new file - unfinished

---
+++ zip_official/zip_official/Frontend_Code/ViewControllers/Events/EditMyEventViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EditMyEventViewController.swift
+//  zip_official
+//
+//  Created by Yianni Zavaliagkos on 1/28/22.
+//
+
+import UIKit
+
+class EditMyEventViewController: UIViewController {
+    var event: Event = Event()
+    var timer: Timer!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private let tableView = UITableView()
+    private let tableHeader = UIView()
+    private let tableFooter = UIView()
+
+
+}
