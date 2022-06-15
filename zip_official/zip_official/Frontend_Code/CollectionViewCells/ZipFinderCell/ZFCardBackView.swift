499812f0e7f68df0b481e96e47246aeec942689f
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-06-15 13:01:50 -0400

    finished and refactored

--- zip_official/zip_official/Frontend_Code/CollectionViewCells/ZipFinderCell/ZFCardBackView.swift
+++ zip_official/zip_official/Frontend_Code/CollectionViewCells/ZipFinderCell/ZFCardBackView.swift
@@ -13,280 +13,89 @@ import DropDown
 
 protocol ZFCardBackDelegate: AnyObject {
     func openProfile(_ user: User)
+    func openZips(_ user: User)
+    func messageUser(_ user: User)
+    func inviteUser(_ user: User)
 }
 
 class ZFCardBackView: UIView {
-    var cellColor = UIColor.zipBlue.withAlphaComponent(1)
     weak var delegate: ZFCardBackDelegate?
     //User
-    private var user = User()
-    var userLoc = CLLocation()
+    private var user: User?
+    var userLoc: CLLocation
 
-    private var dropDownTitles: [String] = []
+    private var dropDownTitles: [String]
     
     //MARK: - Labels
-    private var firstNameLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipBodyBold
-        label.numberOfLines = 1
-        label.text = "A"
-        label.adjustsFontSizeToFitWidth = true
-        label.minimumScaleFactor = 0.5
-        return label
-    }()
-    
-    private var lastNameLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipBodyBold
-        label.numberOfLines = 1
-        label.text = "A"
-        label.adjustsFontSizeToFitWidth = true
-        label.minimumScaleFactor = 0.5
-        return label
-    }()
-    
-    private var ageLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipBodyBold
-        label.text = "A"
-        return label
-    }()
-    
-    private var distanceLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipBodyBold
-        label.text = "A"
-        label.adjustsFontSizeToFitWidth = true
-        label.minimumScaleFactor = 0.5
-        return label
-    }()
-    
-    private var usernameLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipTitle.withSize(20)
-        label.sizeToFit()
-        label.text = "@"
-        return label
-    }()
-    
-    private var bioLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipBody
-        label.sizeToFit()
-        label.text = "@"
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private var schoolLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipBody
-        label.sizeToFit()
-        label.text = "@"
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private var interestsLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipBody
-        label.sizeToFit()
-        label.text = "@"
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    private var joinedDateLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .zipVeryLightGray
-        label.font = .zipBody.withSize(16)
-        label.sizeToFit()
-        label.text = "Joined Zipper on January 1st, 2022"
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        return label
-    }()
+    private var firstNameLabel: UILabel
+    private var lastNameLabel: UILabel
+    private var ageLabel: UILabel
+    private var distanceLabel: DistanceLabel
+    private var usernameLabel: UILabel
+    private var bioLabel: UILabel
+    private var schoolLabel: UILabel
+    private var interestsLabel: UILabel
+    private var joinedDateLabel: UILabel
     
     
-    private let zipsButton: UIButton = {
-        let btn = UIButton()
-        let config = UIImage.SymbolConfiguration(pointSize: 18, weight: .medium, scale: .large) //
-        let img = UIImage(systemName: "person.3", withConfiguration: config)?.withRenderingMode(.alwaysOriginal).withTintColor(.white)
-        btn.setImage(img, for: .normal)
-        btn.backgroundColor = .zipLightGray
-        btn.layer.masksToBounds = true
-        return btn
-    }()
-    
-    private let messageButton: UIButton = {
-        let btn = UIButton()
-        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .medium, scale: .large) //, withConfiguration: config
-        let img = UIImage(systemName: "message", withConfiguration: config)?.withRenderingMode(.alwaysOriginal).withTintColor(.white)
-        btn.setImage(img, for: .normal)
-        btn.backgroundColor = .zipLightGray
-        btn.layer.masksToBounds = true
-        return btn
-    }()
-    
-    private let inviteButton: UIButton = {
-        let btn = UIButton()
-        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .medium, scale: .large) //, withConfiguration: config
-        let img = UIImage(systemName: "paperplane", withConfiguration: config)?.withRenderingMode(.alwaysOriginal).withTintColor(.white)
-        btn.setImage(img, for: .normal)
-        btn.backgroundColor = .zipLightGray
-        btn.layer.masksToBounds = true
-        return btn
-    }()
+    private let zipsButton: IconButton
+    private let messageButton: IconButton
+    private let inviteButton: IconButton
 
-    
     
     //MARK: - Subviews
-    private var profilePicture = UIImageView()
-    private var reportPopUp = DropDown()
-    private var reportButton: UIButton = {
-        let btn = UIButton()
-        btn.setImage(UIImage(named: "report"), for: .normal)
-        btn.addTarget(self, action: #selector(didTapReportButton), for: .touchUpInside)
-        return btn
-    }()
+    private var profilePicture: UIImageView
+    private var reportPopUp: DropDown
+    private var reportButton: UIButton
     
-    let schoolImage = UIImageView(image: UIImage(systemName: "graduationcap.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.white))
-    let interestsImage = UIImageView(image: UIImage(systemName: "star.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.white))
+    let schoolImage: UIImageView
+    let interestsImage: UIImageView
     
-    private var slideView: MTSlideToOpenView = {
-        let slider = MTSlideToOpenView(frame: CGRect(x: 0, y: 0, width: 317, height: 56))
-        slider.thumnailImageView.image = UIImage(named: "zipperSlider")
-        slider.thumnailImageView.backgroundColor = .clear
-        slider.backgroundColor = .zipGray
-        return slider
-    }()
-    
-    //MARK: - Button Actions
-    @objc private func didTapReportButton(){
-        reportPopUp.show()
-        print("report tapped")
-    }
-    
-    @objc private func openProfile(){
-        delegate?.openProfile(user)
-    }
-    
-    @objc private func unrequestUser(){
-        slideView.resetStateWithAnimation(true)
-    }
+    private var slideView: MTSlideToOpenView
     
     
-    public func configure(user: User, cellColor: UIColor, loc: CLLocation, url: URL){
-        backgroundColor = .clear
+    init() {
+        zipsButton = IconButton.zipsIcon()
+        inviteButton = IconButton.inviteIcon()
+        messageButton = IconButton.messageIcon()
+        firstNameLabel = UILabel.zipHeader()
+        lastNameLabel = UILabel.zipSubtitle()
+        usernameLabel = UILabel.zipHeader()
+        ageLabel = UILabel.zipSubtitle2()
+        bioLabel = UILabel.zipTextFill()
+        schoolLabel = UILabel.zipTextFill()
+        joinedDateLabel = UILabel.zipTextDetail()
+        interestsLabel = UILabel.zipTextFill()
+        distanceLabel = DistanceLabel()
+        
+        schoolImage = UIImageView(image: UIImage(systemName: "graduationcap.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.white))
+        interestsImage = UIImageView(image: UIImage(systemName: "star.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.white))
+        profilePicture = UIImageView()
+        
+        slideView = MTSlideToOpenView(frame: CGRect(x: 0, y: 0, width: 317, height: 56))
+        
+        reportPopUp = DropDown()
+            
+        reportButton = UIButton()
+        userLoc = CLLocation()
+        dropDownTitles = []
+        
+        super.init(frame: .zero)
         layer.cornerRadius = 20
-        self.user = user
-        self.cellColor = cellColor
-        self.userLoc = loc
         
-        profilePicture.sd_setImage(with: url, completed: nil)
+        zipsButton.setIconDimension(width: 60)
+        messageButton.setIconDimension(width: 60)
+        inviteButton.setIconDimension(width: 60)
         
-        configureLabels()
-        configureDropDown()
-        configureSlider()
-        addSubviews()
-        configureSubviewLayout()
-        layoutSubviews()
-    }
-    
-    private var countLayout = 0
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-
-    }
-
-
-
-   
-    
-    //MARK: - Configure Labels
-    private func configureLabels(){
-        
+        reportButton.setImage(UIImage(named: "report"), for: .normal)
+        reportButton.addTarget(self, action: #selector(didTapReportButton), for: .touchUpInside)
+         
         if AppDelegate.userDefaults.bool(forKey: "hasHomeButton"){
             usernameLabel.font = .zipBody.withSize(18)
             firstNameLabel.font = .zipTitle.withSize(20)
             lastNameLabel.font = .zipTitle.withSize(20)
             ageLabel.font = .zipTitle.withSize(20)
-            distanceLabel.font =  .zipTitle.withSize(16)
         }
-        
-        usernameLabel.text = "@" + user.username
-//        let name = user.name.components(separatedBy: " ")
-//
-//        firstNameLabel.text = name[0]
-//        lastNameLabel.text = name[1]
-        
-        firstNameLabel.text = user.firstName
-        lastNameLabel.text = user.lastName
-
-        ageLabel.text = "\(user.age) years old"
-        var distance = Double(round(10*(userLoc.distance(from: user.location))/1000))/10
-        var unit = "km"
-        if NSLocale.current.regionCode == "US" {
-            distance = round(10*distance/1.6)/10
-            unit = "miles"
-        }
-        
-        if distance > 10 {
-            let intDistance = Int(distance)
-            if distance <= 1 {
-                if unit == "miles" {
-                    unit = "mile"
-                }
-                distanceLabel.text = "<1 \(unit) away"
-            } else if distance >= 500 {
-                distanceLabel.text = ">500 \(unit) away"
-            } else {
-                distanceLabel.text = String(intDistance) + " \(unit) away"
-            }
-            distanceLabel.textColor = cellColor
-        } else {
-            if distance <= 1 {
-                if unit == "miles" {
-                    unit = "mile"
-                }
-                distanceLabel.text = "<1 \(unit) away"
-            } else if distance >= 500 {
-                distanceLabel.text = ">500 \(unit) away"
-            } else {
-                distanceLabel.text = String(distance) + " \(unit) away"
-            }
-            distanceLabel.textColor = cellColor
-        }
-        
-        
-        
-        
-        
-        bioLabel.text = user.bio
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMMM d, yyyy"
-        
-        if user.school != nil {
-            schoolLabel.text = user.school!
-        }
-        
-        if user.interests.count != 0 {
-            interestsLabel.text = "Interests: " + user.interests.map{$0.description}.joined(separator: ", ")
-        }
-        
         
         let usernameTap = UITapGestureRecognizer(target: self, action: #selector(openProfile))
         usernameLabel.isUserInteractionEnabled = true
@@ -299,14 +108,96 @@ class ZFCardBackView: UIView {
         let lastNameTap = UITapGestureRecognizer(target: self, action: #selector(openProfile))
         lastNameLabel.isUserInteractionEnabled = true
         lastNameLabel.addGestureRecognizer(lastNameTap)
+        
+       
+        
+        interestsLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        interestsImage.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        interestsLabel.numberOfLines = 2
+        interestsLabel.lineBreakMode = .byWordWrapping
+        
+        addSubviews()
+        configureSubviewLayout()
+        configureSlider()
+        configureDropDown()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func didTapZipsButton(){
+        
+    }
+    
+    @objc private func didTapMessageButton() {
+        
+    }
+    
+    @objc private func didTapInviteButton(){
+        
+    }
+    
+    //MARK: - Button Actions
+    @objc private func didTapReportButton(){
+        reportPopUp.show()
+        print("report tapped")
+    }
+    
+    @objc private func openProfile(){
+        guard let user = user else {
+            return
+        }
+        delegate?.openProfile(user)
+    }
+    
+    @objc private func unrequestUser(){
+        slideView.resetStateWithAnimation(true)
+    }
+    
+    
+    public func configure(user: User){
+        print("SUBVIEW COUNT = \(subviews.count)")
+        
+        
+        backgroundColor = .clear
+        self.user = user
+        
+        profilePicture.sd_setImage(with: user.getProfilePicUrl(), completed: nil)
+        usernameLabel.text = "@" + user.username
+        firstNameLabel.text = user.firstName
+        lastNameLabel.text = user.lastName
+        ageLabel.text = "\(user.age) years old"
+        bioLabel.text = user.bio
+        distanceLabel.update(distance: user.distance)
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        
+        if user.school != nil {
+            schoolLabel.text = user.school!
+        }
+        
+        if user.interests.count != 0 {
+            interestsLabel.text = "Interests: " + user.interests.map{$0.description}.joined(separator: ", ")
+        }
+
     }
     
     //MARK: - Configure Slider
     private func configureSlider(){
-        slideView.sliderViewTopDistance = 6
+        slideView.thumnailImageView.image = UIImage(named: "zipperSlider")
+        slideView.thumnailImageView.backgroundColor = .clear
+        slideView.thumbnailViewStartingDistance = -10
+        slideView.backgroundColor = .zipGray
+        
+//        slideView.
+        slideView.sliderViewTopDistance = 11
         slideView.sliderCornerRadius = 15
         slideView.delegate = self
         slideView.sliderTextLabel.text = ""
+        slideView.textLabel.text = ""
         
         slideView.backgroundColor = .clear
 
@@ -341,10 +232,9 @@ class ZFCardBackView: UIView {
     
     //MARK: - Configure Drop Down
     private func configureDropDown(){
-//        let name = user.name.components(separatedBy: " ")
-//        dropDownTitles = ["Report \(name[0])",
-//                          "Block \(name[0])",
-//                          "Don't show me \(name[0])"]
+        guard let user = user else {
+            return
+        }
         
         dropDownTitles = ["Report \(user.firstName)",
                           "Block \(user.firstName)",
@@ -364,8 +254,7 @@ class ZFCardBackView: UIView {
    
     
     //MARK: - Add Subviews
-    private func addSubviews(){
-        //Card FrontView
+    public func addSubviews(){
         addSubview(usernameLabel)
         addSubview(reportButton)
         
@@ -384,17 +273,16 @@ class ZFCardBackView: UIView {
         addSubview(interestsLabel)
         addSubview(joinedDateLabel)
         
-        
         addSubview(zipsButton)
         addSubview(inviteButton)
         addSubview(messageButton)
 
         addSubview(slideView)
     }
-
+    
     
     //MARK: - Add Constraints
-    private func configureSubviewLayout(){
+    func configureSubviewLayout(){
         let buffer = CGFloat(10.0)
 
         // Username label
@@ -402,7 +290,6 @@ class ZFCardBackView: UIView {
         usernameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
         usernameLabel.topAnchor.constraint(equalTo: topAnchor, constant: buffer).isActive = true
 
-        
         //Report Button
         reportButton.translatesAutoresizingMaskIntoConstraints = false
         reportButton.heightAnchor.constraint(equalToConstant: usernameLabel.intrinsicContentSize.height*1.5).isActive = true
@@ -444,26 +331,18 @@ class ZFCardBackView: UIView {
         zipsButton.translatesAutoresizingMaskIntoConstraints = false
         zipsButton.topAnchor.constraint(equalTo: firstNameLabel.topAnchor).isActive = true
         zipsButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -20).isActive = true
-        zipsButton.heightAnchor.constraint(equalToConstant:  60).isActive = true
-        zipsButton.widthAnchor.constraint(equalTo: zipsButton.heightAnchor).isActive = true
+        zipsButton.widthAnchor.constraint(equalToConstant:  60).isActive = true
         
         messageButton.translatesAutoresizingMaskIntoConstraints = false
         messageButton.topAnchor.constraint(equalTo: zipsButton.bottomAnchor, constant: 30).isActive = true
         messageButton.rightAnchor.constraint(equalTo: zipsButton.rightAnchor).isActive = true
-        messageButton.heightAnchor.constraint(equalTo: zipsButton.heightAnchor).isActive = true
-        messageButton.widthAnchor.constraint(equalTo: messageButton.heightAnchor).isActive = true
+        messageButton.widthAnchor.constraint(equalTo: zipsButton.widthAnchor).isActive = true
         
         inviteButton.translatesAutoresizingMaskIntoConstraints = false
         inviteButton.topAnchor.constraint(equalTo: messageButton.bottomAnchor, constant: 30).isActive = true
         inviteButton.rightAnchor.constraint(equalTo: zipsButton.rightAnchor).isActive = true
-        inviteButton.heightAnchor.constraint(equalTo: zipsButton.heightAnchor).isActive = true
-        inviteButton.widthAnchor.constraint(equalTo: inviteButton.heightAnchor).isActive = true
+        inviteButton.widthAnchor.constraint(equalTo: zipsButton.widthAnchor).isActive = true
         
-        
-        zipsButton.layer.cornerRadius = 30
-        inviteButton.layer.cornerRadius = 30
-        messageButton.layer.cornerRadius = 30
-
         
         // School Label
         schoolImage.translatesAutoresizingMaskIntoConstraints = false
@@ -480,6 +359,8 @@ class ZFCardBackView: UIView {
         interestsImage.translatesAutoresizingMaskIntoConstraints = false
         interestsImage.leftAnchor.constraint(equalTo: usernameLabel.leftAnchor).isActive = true
         interestsImage.topAnchor.constraint(equalTo: schoolImage.bottomAnchor, constant: 30).isActive = true
+        interestsImage.widthAnchor.constraint(equalTo: interestsImage.heightAnchor).isActive = true
+        
         
         interestsLabel.translatesAutoresizingMaskIntoConstraints = false
         interestsLabel.centerYAnchor.constraint(equalTo: interestsImage.centerYAnchor).isActive = true
