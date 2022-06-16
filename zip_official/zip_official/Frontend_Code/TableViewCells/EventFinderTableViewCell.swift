a973a89d48864455f731ea06c10bf980f4f68581
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-06-16 17:18:39 -0400

    refactored

--- zip_official/zip_official/Frontend_Code/TableViewCells/EventFinderTableViewCell.swift
+++ zip_official/zip_official/Frontend_Code/TableViewCells/EventFinderTableViewCell.swift
@@ -14,85 +14,23 @@ class EventFinderTableViewCell: UITableViewCell {
     weak var delegate: UpdateZipRequestsTableDelegate?
     
     
-    var event = Event()
-    var userLoc = CLLocation(latitude: 0, longitude: 0)
+    var event: Event?
     
     //MARK: - Subviews
-    private let bgView = UIView()
-    private let eventImage: UIImageView = {
-        let imgView = UIImageView()
-        imgView.isUserInteractionEnabled = true
-        return imgView
-    }()
+    private let bgView: UIView
+    private let eventImage: UIImageView
     
     // Buttons
-    private let goingButton: UIButton = {
-        let btn = UIButton()
-        let largeConfig = UIImage.SymbolConfiguration(pointSize: 22, weight: .bold, scale: .large)
-        let img = UIImage(systemName: "checkmark.circle.fill", withConfiguration: largeConfig)?
-                        .withRenderingMode(.alwaysOriginal)
-                        .withTintColor(.zipYellow)
-        btn.setImage(img, for: .normal)
-        return btn
-    }()
-    
-    private let interestedButton: UIButton = {
-        let btn = UIButton()
-//        btn.layer.masksToBounds = true
-//        btn.layer.cornerRadius = 20
-        let largeConfig = UIImage.SymbolConfiguration(pointSize: 22, weight: .bold, scale: .large)
-        let img = UIImage(systemName: "xmark.circle.fill", withConfiguration: largeConfig)?
-                        .withRenderingMode(.alwaysOriginal)
-                        .withTintColor(.zipVeryLightGray)
-        btn.setImage(img, for: .normal)
-        return btn
-    }()
+    private let goingButton: UIButton
+    private let interestedButton: UIButton
     
     //Labels
-    private var titleLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .zipTitle.withSize(18)
-        label.numberOfLines = 1
-        label.textAlignment = .left
-        label.sizeToFit()
-        label.text = "A"
-        return label
-    }()
+    private var titleLabel: UILabel
+    private let participantsLabel: UILabel
+    private var distanceLabel: DistanceLabel
+    private var dateLabel: IconLabel
+    private var timeLabel: IconLabel
     
-    private let participantsLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .zipVeryLightGray
-        label.font = .zipBody.withSize(16)
-        label.textAlignment = .right
-        label.numberOfLines = 2
-        label.sizeToFit()
-        return label
-    }()
-    
-    private var distanceLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .zipVeryLightGray
-        label.font = .zipBody.withSize(16)
-        label.sizeToFit()
-        return label
-    }()
-    
-    private var dateLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .zipVeryLightGray
-        label.font = .zipBody.withSize(16)
-        label.sizeToFit()
-        return label
-    }()
-    
-    private var timeLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .zipVeryLightGray
-        label.font = .zipBody.withSize(16)
-        label.sizeToFit()
-        return label
-    }()
     
     @objc private func openEvent(){
         
@@ -116,89 +54,125 @@ class EventFinderTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
     
-    //MARK: - Config
-    public func configure(_ event: Event, loc: CLLocation){
-        backgroundColor = .clear
-        self.event = event
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        self.bgView = UIView()
+        self.eventImage = UIImageView()
+        self.goingButton = UIButton()
+        self.interestedButton = UIButton()
+        self.titleLabel = UILabel.zipTextNotiBold()
+        self.participantsLabel = UILabel.zipTextDetail()
         
-        guard let coordinates = UserDefaults.standard.object(forKey: "userLoc") as? [Double] else {
-            return
-        }
+        let smallConfig = UIImage.SymbolConfiguration(pointSize: 14, weight: .bold, scale: .small)
+
+        self.distanceLabel = DistanceLabel(labelFont: .zipTextDetail, color: .zipVeryLightGray, config: smallConfig)
+        self.timeLabel = IconLabel(iconImage: UIImage(systemName: "clock.fill")?.withConfiguration(smallConfig), labelFont: .zipTextDetail, color: .zipVeryLightGray)
+        self.dateLabel = IconLabel(iconImage: UIImage(systemName: "calendar")?.withConfiguration(smallConfig), labelFont: .zipTextDetail, color: .zipVeryLightGray)
+
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        eventImage.isUserInteractionEnabled = true
+
         
-        userLoc = CLLocation(latitude: coordinates[0], longitude: coordinates[1])
-        interestedButton.addTarget(self, action: #selector(didTapInterestedButton), for: .touchUpInside)
-        goingButton.addTarget(self, action: #selector(didTapGoingButton), for: .touchUpInside)
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 22, weight: .bold, scale: .large)
+        let goingIcon = UIImage(systemName: "checkmark.circle.fill", withConfiguration: largeConfig)?
+            .withRenderingMode(.alwaysOriginal)
+            .withTintColor(.zipYellow)
+        goingButton.setImage(goingIcon, for: .normal)
+        
+ 
+        let interestedIcon = UIImage(systemName: "xmark.circle.fill", withConfiguration: largeConfig)?
+            .withRenderingMode(.alwaysOriginal)
+            .withTintColor(.zipVeryLightGray)
+        interestedButton.setImage(interestedIcon, for: .normal)
         
         configureBackground()
         addSubviews()
+        configureSubviewLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Config
+    public func configure(_ event: Event){
+        backgroundColor = .clear
+        self.event = event
+        
+        interestedButton.addTarget(self, action: #selector(didTapInterestedButton), for: .touchUpInside)
+        goingButton.addTarget(self, action: #selector(didTapGoingButton), for: .touchUpInside)
+        
         configureImage()
         configureLabels()
-        configureSubviewLayout()
     }
     
     //MARK: - Background Config
     private func configureBackground(){
         contentView.addSubview(bgView)
-        bgView.frame = CGRect(x: 5, y: 5, width: contentView.frame.width-10, height: contentView.frame.height-10)
+        bgView.translatesAutoresizingMaskIntoConstraints = false
+        bgView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 5).isActive = true
+        bgView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -5).isActive = true
+        bgView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5).isActive = true
+        bgView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5).isActive = true
+
         bgView.layer.cornerRadius = 10
         bgView.backgroundColor = .zipLightGray
     }
     
     //MARK: - Label Config
     private func configureLabels(){
+        guard let event = event else {
+            return
+        }
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(openEvent))
         titleLabel.isUserInteractionEnabled = true
         titleLabel.addGestureRecognizer(tap)
         
         titleLabel.text = event.title
+        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMM d"
-        dateLabel.text = dateFormatter.string(from: event.startTime)
+        dateLabel.update(string:  " " + dateFormatter.string(from: event.startTime))
         
         dateFormatter.dateFormat = "h:mm a"
-        timeLabel.text = dateFormatter.string(from: event.startTime)
+        timeLabel.update(string: " " + dateFormatter.string(from: event.startTime))
         
         let eventLoc = CLLocation(latitude: event.coordinates.latitude, longitude: event.coordinates.longitude)
-        var distance = Double(round(10*(userLoc.distance(from: eventLoc))/1000))/10
-        var unit = "km"
-        if NSLocale.current.regionCode == "US" {
-            distance = round(10*distance/1.6)/10
-            unit = "miles"
-        }
-        if distance > 10 {
-            distanceLabel.text = String(Int(distance)) + " " + unit
-        } else {
-            distanceLabel.text = String(distance) + " " + unit
-
-        }
-
+        distanceLabel.update(location: eventLoc)
+        
+        
         participantsLabel.text = String(event.usersGoing.count) + "/" + String(event.maxGuests) + "\ngoing"
         
-        participantsLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        participantsLabel.textAlignment = .right
+        
+        participantsLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         titleLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
 
     }
     
     //MARK: - Image Config
     private func configureImage() {
+        guard let event = event else {
+            return
+        }
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(openEvent))
         eventImage.addGestureRecognizer(tap)
         
         eventImage.image = event.image
-        let size = contentView.frame.height/2
-        let y = bgView.frame.height/2 - size/2
-        eventImage.frame = CGRect(x: 10,
-                                  y: y,
-                                  width: size,
-                                  height: size)
 
+        eventImage.translatesAutoresizingMaskIntoConstraints = false
+        eventImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        eventImage.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.5).isActive = true
+        eventImage.leftAnchor.constraint(equalTo: bgView.leftAnchor, constant: 10).isActive = true
+        eventImage.widthAnchor.constraint(equalTo: eventImage.heightAnchor).isActive = true
+        
         eventImage.layer.masksToBounds = true
-        eventImage.layer.cornerRadius = size/2
+        eventImage.layer.cornerRadius = contentView.frame.height/4
         
         eventImage.layer.borderWidth = 2
         eventImage.layer.borderColor = UIColor.zipYellow.cgColor
-
-
     }
     
     //MARK: - Add Subviews
@@ -228,58 +202,29 @@ class EventFinderTableViewCell: UITableViewCell {
         titleLabel.leftAnchor.constraint(equalTo: eventImage.rightAnchor, constant: 10).isActive = true
         titleLabel.rightAnchor.constraint(equalTo: participantsLabel.leftAnchor, constant: -5).isActive = true
 
-        let distanceImage = UIImageView(image: UIImage(named: "distanceToWhite")?.withTintColor(.zipVeryLightGray))
-        let dateImage = UIImageView(image: UIImage(systemName: "calendar")?.withRenderingMode(.alwaysOriginal).withTintColor(.zipVeryLightGray))
-        let timeImage = UIImageView(image: UIImage(systemName: "clock.fill")?.withRenderingMode(.alwaysOriginal).withTintColor(.zipVeryLightGray))
-
-        bgView.addSubview(distanceImage)
-        bgView.addSubview(dateImage)
-        bgView.addSubview(timeImage)
-        
         distanceLabel.translatesAutoresizingMaskIntoConstraints = false
         distanceLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5).isActive = true
-        distanceLabel.leftAnchor.constraint(equalTo: distanceImage.rightAnchor, constant: 5).isActive = true
-
-        distanceImage.translatesAutoresizingMaskIntoConstraints = false
-        distanceImage.centerYAnchor.constraint(equalTo: distanceLabel.centerYAnchor).isActive = true
-        distanceImage.leftAnchor.constraint(equalTo: titleLabel.leftAnchor).isActive = true
-        distanceImage.heightAnchor.constraint(equalToConstant: distanceLabel.intrinsicContentSize.height).isActive = true
-        distanceImage.widthAnchor.constraint(equalTo: distanceImage.heightAnchor).isActive = true
+        distanceLabel.leftAnchor.constraint(equalTo: titleLabel.leftAnchor).isActive = true
 
         dateLabel.translatesAutoresizingMaskIntoConstraints = false
         dateLabel.topAnchor.constraint(equalTo: distanceLabel.bottomAnchor).isActive = true
         dateLabel.leftAnchor.constraint(equalTo: distanceLabel.leftAnchor).isActive = true
 
-        dateImage.translatesAutoresizingMaskIntoConstraints = false
-        dateImage.centerYAnchor.constraint(equalTo: dateLabel.centerYAnchor).isActive = true
-        dateImage.centerXAnchor.constraint(equalTo: distanceImage.centerXAnchor).isActive = true
-        dateImage.heightAnchor.constraint(equalTo: distanceImage.heightAnchor).isActive = true
-        dateImage.widthAnchor.constraint(equalTo: dateImage.heightAnchor).isActive = true
-
         timeLabel.translatesAutoresizingMaskIntoConstraints = false
         timeLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor).isActive = true
         timeLabel.leftAnchor.constraint(equalTo: distanceLabel.leftAnchor).isActive = true
-
-        timeImage.translatesAutoresizingMaskIntoConstraints = false
-        timeImage.centerYAnchor.constraint(equalTo: timeLabel.centerYAnchor).isActive = true
-        timeImage.centerXAnchor.constraint(equalTo: distanceImage.centerXAnchor).isActive = true
-        timeImage.heightAnchor.constraint(equalTo: distanceImage.heightAnchor).isActive = true
-        timeImage.widthAnchor.constraint(equalTo: timeImage.heightAnchor).isActive = true
         
         participantsLabel.translatesAutoresizingMaskIntoConstraints = false
         participantsLabel.topAnchor.constraint(equalTo: titleLabel.topAnchor).isActive = true
-        participantsLabel.rightAnchor.constraint(equalTo: bgView.rightAnchor, constant: -5).isActive = true
+        participantsLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10).isActive = true
 
         goingButton.translatesAutoresizingMaskIntoConstraints = false
-        goingButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10).isActive = true
+        goingButton.rightAnchor.constraint(equalTo: participantsLabel.rightAnchor).isActive = true
         goingButton.topAnchor.constraint(equalTo: participantsLabel.bottomAnchor, constant: 10).isActive = true
         
         interestedButton.translatesAutoresizingMaskIntoConstraints = false
         interestedButton.topAnchor.constraint(equalTo: goingButton.topAnchor).isActive = true
-        interestedButton.rightAnchor.constraint(equalTo: goingButton.leftAnchor, constant: -10).isActive = true
+        interestedButton.rightAnchor.constraint(equalTo: goingButton.leftAnchor, constant: -7).isActive = true
 
-
-        
-        
     }
 }
