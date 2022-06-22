14bbf793a54b66767b3b8015e7c17f5aab79eb47
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-06-22 18:00:42 -0400

    new

---
+++ zip_official/zip_official/Frontend_Code/TableViewCells/EventSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventSearchTableViewCell.swift
+//  zip_official
+//
+//  Created by Yianni Zavaliagkos on 6/17/22.
+//
+
+import UIKit
+
+class EventSearchTableViewCell: UITableViewCell {
+    static let identifier = "eventSearch"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
