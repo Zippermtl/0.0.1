7a2b06e5e69c96bbc325161bf87c82d0282baf15
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-06-22 17:59:30 -0400

    dark mode

--- zip_official/zip_official/Frontend_Code/Delegates/SceneDelegate.swift
+++ zip_official/zip_official/Frontend_Code/Delegates/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
+        window?.overrideUserInterfaceStyle = .dark
         window?.makeKeyAndVisible()
 
 //        window?.rootViewController = SMSCodeViewController()
