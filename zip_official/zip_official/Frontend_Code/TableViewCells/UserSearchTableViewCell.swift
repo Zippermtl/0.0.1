e4dfcad4f0ad23cc77f7bcb759a5f4269ab98df0
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-06-22 18:00:49 -0400

    todo: new

---
+++ zip_official/zip_official/Frontend_Code/TableViewCells/UserSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserSearchTableViewCell.swift
+//  zip_official
+//
+//  Created by Yianni Zavaliagkos on 6/17/22.
+//
+
+import UIKit
+
+class UserSearchTableViewCell: UITableViewCell {
+    static let identifier = "userSearch"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
