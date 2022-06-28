e35e15c717b70225d98091acd774446f2068e859
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-06-28 17:25:44 -0400

    UI changes

--- zip_official/zip_official/Frontend_Code/IconLabel.swift
+++ zip_official/zip_official/Frontend_Code/IconLabel.swift
@@ -12,27 +12,38 @@ class IconLabel: UILabel {
     private let icon: NSTextAttachment
     private var labelFont: UIFont
     private var color: UIColor
+    private var labelText: String
     
     init(iconImage: UIImage?){
         self.labelFont = .zipBody
         self.color = .zipBlue
+        self.labelText = ""
+        
         icon = NSTextAttachment()
-        icon.image = iconImage?.withRenderingMode(.alwaysOriginal).withTintColor(self.color)
         
         super.init(frame: .zero)
+        guard let iconImage = iconImage else {
+            return
+        }
+
+        icon.image = iconImage.withRenderingMode(.alwaysOriginal).withTintColor(self.color)
     }
     
     init(iconImage: UIImage?, labelFont: UIFont, color: UIColor) {
         self.labelFont = labelFont
         self.color = color
+        self.labelText = ""
         icon = NSTextAttachment()
-
-        icon.image = iconImage?.withRenderingMode(.alwaysOriginal).withTintColor(color)
-
         
         super.init(frame: .zero)
+        guard let iconImage = iconImage else {
+            return
+        }
+
+        icon.image = iconImage.withRenderingMode(.alwaysOriginal).withTintColor(self.color)
+        
         textColor = color
-        update(string: "")
+        update(string: labelText)
     }
     
     required init?(coder: NSCoder) {
@@ -41,11 +52,16 @@ class IconLabel: UILabel {
     
     
     public func update(string s: String){
+        labelText = s
         let attachmentString = NSAttributedString(attachment: icon)
         let completeString = NSMutableAttributedString(string: "")
         completeString.append(attachmentString)
         completeString.append(NSAttributedString(string: s, attributes: [NSAttributedString.Key.font: self.labelFont,
                                                                          NSAttributedString.Key.foregroundColor: self.color]))
         attributedText = completeString
+    }
+    
+    public func setIcon(newIcon: UIImage?) {
+        icon.image = newIcon
     }
 }
