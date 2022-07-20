884e405abd943e3d764cd907ca61d08d7a347c5e
Yianni Zavaliagkos <zavaliagkosyianni@gmail.com>
2022-07-19 21:15:07 -0400

    firestore changes

--- zip_official/zip_official/Frontend_Code/Objects/Notification.swift
+++ zip_official/zip_official/Frontend_Code/Objects/Notification.swift
@@ -8,56 +8,64 @@
 import Foundation
 import UIKit
 
-enum NotificationType: Int, CaseIterable {
-    case news
-    case eventPublic
-    case eventInvite
-    case eventTimeChange
-    case eventAddressChange
-    case eventLimitedSpots
-    case zipRequest
-    case zipAccepted
+//enum NotificationType: Int, CaseIterable {
+//    case news
+//    case eventPublic
+//    case eventInvite
+//    case eventTimeChange
+//    case eventAddressChange
+//    case eventLimitedSpots
+//    case zipRequest
+//    case zipAccepted
+//}
+
+public enum NotificationType: CaseIterable {
+    case general
+    case zipNotification
+    case messageNotification
+    case EventNotification
+    
+    var subtypes: [NotificationSubtype] {
+        switch self {
+        case .general: return [.pause_all, .news_update]
+        case .zipNotification: return [.zip_request, .accepted_zip_request]
+        case .messageNotification: return [.message, .message_request]
+        case .EventNotification: return [.event_invite, .public_event, .one_day_reminder, .change_to_event_info]
+        }
+    }
+}
+
+public enum NotificationSubtype: Int, Codable, CaseIterable{
+    case pause_all = 0
+    case news_update = 1
+    case zip_request = 2
+    case accepted_zip_request = 3
+    case message = 4
+    case message_request = 5
+    case event_invite = 6
+    case public_event = 7
+    case one_day_reminder = 8
+    case change_to_event_info = 9
 }
 
 
-/// All notification keys
-public let ALL_NOTIF_KEYS = [
-    "pause_all", "news_update",
-    "zip_requests", "accepted_zip_requests",
-    "messages", "message_requests",
-    "event_invites", "public_events",
-    "one_day_reminders", "changes_to_event_info"
-]
 
 
-/// For you Yianni, to make your life simpler I grouped notification keys below by category
-
-/// Notification keys under the "General" section
-public let GENERAL_NOTIF_KEYS = [ALL_NOTIF_KEYS[0], ALL_NOTIF_KEYS[1]]
-
-/// Notification keys under the "Zips" section
-public let ZIPS_NOTIF_KEYS = [ALL_NOTIF_KEYS[2], ALL_NOTIF_KEYS[3]]
-
-/// Notification keys under the "Messages" section
-public let MESSAGES_NOTIF_KEYS = [ALL_NOTIF_KEYS[4], ALL_NOTIF_KEYS[5]]
-
-/// Notification kets under the "Events" section
-public let EVENTS_NOTIF_KEYS = [ALL_NOTIF_KEYS[6], ALL_NOTIF_KEYS[7], ALL_NOTIF_KEYS[8], ALL_NOTIF_KEYS[9]]
 
 
 let DEFAULT_NOTIF_VALUE = 0 // Disable notifications by default
 
 
 // Decode integer into a dictionary
-public func DecodePreferences(_ N: Int?) -> [String: Bool] {
-    var values: [String: Bool] = [:]
+public func DecodePreferences(_ N: Int?) -> NotificationPreference {
+    var values: NotificationPreference = [:]
     var counter = 0 // Start index
     var current = N ?? DEFAULT_NOTIF_VALUE
-    let NB_PREFS = ALL_NOTIF_KEYS.count
+    let NB_PREFS = NotificationSubtype.allCases.count
     
     // While we have not yet created an array of NUMBER_OF_PREFERENCES elements
     while (counter < NB_PREFS) {
-        values[ALL_NOTIF_KEYS[NB_PREFS-1-counter]] = current%2 > 0
+        values[NotificationSubtype.allCases[NB_PREFS-1-counter]] = current%2 > 0
         current /= 2 // Get rid of LSB
         counter += 1 // Increase counter
     }
@@ -68,13 +76,12 @@ public func DecodePreferences(_ N: Int?) -> [String: Bool] {
 
 
 // Encode preferences into a integer
-public func EncodePreferences(_ preferences: [String: Bool]) -> Int {
+public func EncodePreferences(_ preferences: NotificationPreference) -> Int {
     var total = 0 // Total value
     var powerOfTwo = 1 // Start with 2^0
     
-    // Loop through each value starting from end
-    for i in (0..<ALL_NOTIF_KEYS.count).reversed() {
-        let pref = preferences[ALL_NOTIF_KEYS[i]]
+    for i in (0..<NotificationSubtype.allCases.count).reversed() {
+        let pref = preferences[NotificationSubtype.allCases[i]]
         if let preference = pref {
             if preference {total += powerOfTwo} // If bit is on, add power of 2
         }
