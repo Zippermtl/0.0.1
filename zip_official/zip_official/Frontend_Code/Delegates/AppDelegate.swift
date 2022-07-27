f30ab085bdc5448bc92494c0ff69e9e920d11fb6
Gabe <gabe2002denton@gmail.com>
2022-07-27 01:32:46 -0400

    added caleb's code

--- zip_official/zip_official/Frontend_Code/Delegates/AppDelegate.swift
+++ zip_official/zip_official/Frontend_Code/Delegates/AppDelegate.swift
@@ -190,6 +190,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
+    func registerForPushNotifications() {
+      //1
+        UNUserNotificationCenter.current()
+          .requestAuthorization(
+            options: [.alert, .sound, .badge]) { [weak self] granted, _ in
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+            self?.getNotificationSettings()
+          }
+    }
+    func getNotificationSettings() {
+      UNUserNotificationCenter.current().getNotificationSettings { settings in
+        print("Notification settings: \(settings)")
+          
+        guard settings.authorizationStatus == .authorized else { return }
+        DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    func application(
+         _ application: UIApplication,
+         didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+       ) {
+         let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+         let token = tokenParts.joined()
+         print("Device Token: \(token)")
+           AppDelegate.userDefaults.set(token, forKey: "deviceId")
+        // GABE THIS IS THE TOKEN I NEED UPLOADED TO FIREBASE UNDER DEVICE_ID
+       }
+    
+    func application(
+          _ application: UIApplication,
+          didFailToRegisterForRemoteNotificationsWithError error: Error
+        ) {
+          print("Failed to register: \(error)")
+        }
     
 }
 
