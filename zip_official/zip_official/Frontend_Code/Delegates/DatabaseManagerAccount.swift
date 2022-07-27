bde31b3d9785ed9b562cc422a08889afae2d4d02
Gabe <gabe2002denton@gmail.com>
2022-07-27 01:47:00 -0400

    cleaning

---
+++ zip_official/zip_official/Frontend_Code/Delegates/DatabaseManagerAccount.swift
@@ -0,0 +1,291 @@
+//
+//  DatabaseManagerAccount.swift
+//  zip_official
+//
+//  Created by user on 7/27/22.
+//
+
+import Foundation
+import FirebaseDatabase
+import MessageKit
+import FirebaseAuth
+import CoreLocation
+import FirebaseFirestore
+
+//MARK: - Account Management
+extension DatabaseManager {
+    /// checks if user exists for given email
+    /// parameters
+    /// - `email`: Target email to be checked
+    ///  - `completion`: async clusire to return with result
+    public func userExists(with userId: String, completion: @escaping (Bool) -> Void) {
+        firestore.collection("AllUserIds").document(userId).getDocument { (document, error) in
+            guard let document = document else {
+                completion(false)
+                return
+            }
+            if document.exists {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: User, completion: @escaping (Error?) -> Void) {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        let joinDate =  formatter.string(from: Date())
+        let deviceId = UIDevice.current.identifierForVendor!.uuidString
+        
+        let userData : [String:Any] = [
+            "id": user.userId,
+            "username": user.username,
+            "firstName": user.firstName,
+            "lastName": user.lastName,
+            "birthday": Timestamp(date: user.birthday),
+            "notifications": EncodePreferences(user.notificationPreferences),
+            "picNum": user.picNum,
+            "school": "",
+            "joinDate": joinDate,
+            "deviceId": [user.deviceId],
+            "bio" : "",
+            "interests" : []
+        ]
+        
+        
+        firestore.collection("UserProfiles").document("\(user.userId)").setData(userData)  { [weak self] error in
+            guard let strongSelf = self,
+                  error == nil else {
+                print("failure to create user with id: \(user.userId) to FireStore")
+                completion(error)
+                return
+            }
+            
+            strongSelf.firestore.collection("AllUserIds").document("\(user.userId)").setData([user.userId:user.fullName])
+            
+            AppDelegate.userDefaults.set(user.userId, forKey: "userId")
+            AppDelegate.userDefaults.set(user.username, forKey: "username")
+            AppDelegate.userDefaults.set(user.fullName, forKey: "name")
+            AppDelegate.userDefaults.set(user.firstName, forKey: "firstName")
+            AppDelegate.userDefaults.set(user.lastName, forKey: "lastName")
+            AppDelegate.userDefaults.set(user.birthday, forKey: "birthday")
+            AppDelegate.userDefaults.set(1, forKey: "picNum")
+            
+            let emptyFriendships: [String: Int]  = [:]
+            AppDelegate.userDefaults.set(emptyFriendships, forKey:  "friendships")
+            AppDelegate.userDefaults.set(EncodePreferences(user.notificationPreferences), forKey: "encodedNotificationSettings")
+
+            AppDelegate.userDefaults.setValue(2, forKey: "maxRangeFilter")
+
+            if user.pictures.count == 0 {
+                //TODO: add default image
+                AppDelegate.userDefaults.set("", forKey: "profilePictureUrl")
+                completion(nil)
+            } else {
+                let image = user.pictures[0]
+                guard let data = image.pngData() else {
+                    return
+                }
+                
+                StorageManager.shared.uploadProfilePicture(with: data, fileName: user.profilePictureFileName, completion: {results in
+                    switch results {
+                    case .success(let downloadUrl):
+                        AppDelegate.userDefaults.set(downloadUrl.description, forKey: "profilePictureUrl")
+                        completion(nil)
+                    case .failure(let error):
+                        print("Storage Manager Error: \(error)")
+                        completion(error)
+                    }
+                })
+            }
+        }
+    }
+    
+    public func createUserLookUp(location: CLLocation, completion: @escaping (Error?) -> Void){
+        guard let userId = AppDelegate.userDefaults.value(forKey: "userId") as? String,
+              let name = AppDelegate.userDefaults.value(forKey: "firstName") as? String,
+              let last = AppDelegate.userDefaults.value(forKey: "lastName") as? String else {
+                  return
+              }
+        
+        firestore.collection("UserFastInfo").document("\(userId)").setData([
+            "lat": location.coordinate.latitude,
+            "long": location.coordinate.longitude,
+            "name": name + " " + last
+        ]) { error in
+            guard error == nil  else {
+                completion(error)
+                return
+            }
+            completion(nil)
+        }
+    }
+    
+    public func updateLocationUserLookUp(location: CLLocation, completion: @escaping (Error?) -> Void){
+        guard let userId = AppDelegate.userDefaults.value(forKey: "userId") as? String else {
+            return
+        }
+        firestore.collection("UserFastInfo").document("\(userId)").updateData([
+            "lat": location.coordinate.latitude,
+            "long": location.coordinate.longitude
+        ]) { error in
+            guard error == nil else{
+                print("failed to write to database")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }
+    }
+    /// Updates the full user profile
+    public func updateUser(with user: User, completion: @escaping (Error?) -> Void) {
+        
+        let userData: [String:Any] = [
+            "id": user.userId,
+            "username": user.username,
+            "firstName": user.firstName,
+            "lastName": user.lastName,
+            "bio": user.bio,
+            "school": user.school ?? "",
+            "picNum": user.picNum,
+            "interests": user.interests.map{ $0.rawValue },
+            "notifications": EncodePreferences(user.notificationPreferences),
+            "deviceId": user.deviceId
+        ]
+        
+        firestore.collection("UserProfiles").document(user.userId).updateData(userData) { error in
+            guard error == nil else{
+                print("failed to write to database")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }
+    }
+    
+    public func updateDeviceId(devId: String, completion: @escaping (Error?) -> Void) {
+        let userData: [String:Any] = [
+            "deviceId": AppDelegate.userDefaults.value(forKey: "deviceId"),
+        ]
+        
+        firestore.collection("UserProfiles").document(AppDelegate.userDefaults.value(forKey: "userId") as! String).updateData(userData) { error in
+            guard error == nil else{
+                print("failed to write to database")
+                completion(error)
+                return
+            }
+            completion(nil)
+        }
+    }
+    
+                                                                           
+    /// Gets all users from Firebase
+    ///  Parameters
+    ///   `completion`: async closure to deal with return result
+    public func getAllUserIds(completion: @escaping (Result<[String: Any], Error>) -> Void) {
+        firestore.collection("AllUserIds").getDocuments() { (querySnapshot,error) in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+
+            var out : [String:Any] = [:]
+            for document in querySnapshot!.documents {
+                document.data().forEach { (key, value) in out[key] = value }
+            }
+            
+            completion(.success(out))
+        }
+    }
+    
+    public enum DatabaseError: Error {
+        case failedToFetch
+    }
+    
+}
+
+//MARK: - User Data Retreival
+extension DatabaseManager {
+    public func loadUserProfile(given user: User, completion: @escaping (Result<User, Error>) -> Void) {
+        firestore.collection("UserProfiles").document(user.userId).getDocument(as: UserCoder.self)  { result in
+            switch result {
+            case .success(let userCoder):
+                print("USER12345", userCoder.bio)
+                userCoder.updateUser(user)
+                print("USER12345", user.bio)
+
+                let imagesPath = "images/" + user.userId
+                StorageManager.shared.getAllImagesManually(path: imagesPath, picNum: user.picNum, completion: { result in
+                    switch result {
+                    case .success(let url):
+                        user.pictureURLs = url
+                        print("Successful pull of user image URLS for \(user.fullName) with \(user.pictureURLs.count) URLS ")
+                        print(user.pictureURLs)
+                        completion(.success(user))
+
+                    case .failure(let error):
+                        print("error load in LoadUser image URLS -> LoadUserProfile -> LoadImagesManually \(error)")
+                    }
+                        
+                })
+                
+            case .failure(let error):
+                print("failed to load user \(user.userId): \(error)")
+            }
+        }
+    }
+    
+    public func loadUserProfileSubView(given id: String, completion: @escaping (Result<User, Error>) -> Void) {
+        firestore.collection("UserFastInfo").document(id).getDocument() { (document,error) in
+//            print("document = ", document)
+//            print("document.data() = ", document?.data())
+
+            guard let document = document,
+                  let docData = document.data() as? [String:[String:Any]],
+                  let data = docData["id"] else {
+//                completion(.failure(error!))
+                return
+            }
+            
+            let user = User(userId: id)
+            let names = (data["name"] as! String).components(separatedBy: " ")
+            user.firstName = names[0]
+            user.lastName = names[1]
+            user.location = CLLocation(latitude: data["lat"] as! Double, longitude: data["long"] as! Double)
+            
+            let imagesPath = "images/" + id
+            StorageManager.shared.getProfilePicture(path: imagesPath, completion: { result in
+                switch result {
+                case .success(let url):
+                    if user.pictureURLs.count > 0 {
+                        user.pictureURLs[0] = url
+                    } else {
+                        user.pictureURLs.append(url)
+                    }
+                    
+                    print("Successful pull of user image URLS for \(user.fullName) with \(user.pictureURLs.count) URLS ")
+                    print(user.pictureURLs)
+                    completion(.success(user))
+
+                case .failure(let error):
+                    print("error load in LoadUser image URLS -> LoadUserProfile -> LoadImagesManually \(error)")
+                }
+                    
+            })
+        }
+    }
+    
+    public func updatePicNum(id: String, picNum: Int, completion: @escaping (Error?) -> Void) {
+        firestore.collection("UserProfiles").document(id).updateData(["picNum" : picNum]) { error in
+            guard error == nil else {
+                completion(error)
+                return
+            }
+            completion(nil)
+        }
+    }
+    
+}
+
