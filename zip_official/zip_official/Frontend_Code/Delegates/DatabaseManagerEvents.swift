42aaa76b06df255b252c653f2ece755302bd2ac2
Gabe <gabe2002denton@gmail.com>
2022-07-29 01:56:37 -0400

    test code

--- zip_official/zip_official/Frontend_Code/Delegates/DatabaseManagerEvents.swift
+++ zip_official/zip_official/Frontend_Code/Delegates/DatabaseManagerEvents.swift
@@ -337,6 +337,56 @@ extension DatabaseManager {
         
     }
     
+    public func createSampleEventsMany(){
+        var eventIdStart = 111
+        var events: [Event] = []
+        var invite = true
+        for i in 0...50 {
+            if((i % 2) == 0){
+                invite = true
+            }
+            if(invite){
+                events.append(Event(eventId: String(eventIdStart), title: "test", coordinates: CLLocation(latitude: 0, longitude: 0), hosts: [User(userId: "u6502222222")], description: "test", address: "fuck my butt", locationName: "fuck my ass", maxGuests: 69, usersGoing: [], usersInterested: [], usersInvite: [User(userId: "u6501111111")], startTime: Date(), endTime: Date()))
+            } else {
+                events.append(Event(eventId: String(eventIdStart), title: "test", coordinates: CLLocation(latitude: 0, longitude: 0), hosts: [User(userId: "u6502222222")], description: "test", address: "fuck my butt", locationName: "fuck my ass", maxGuests: 69, usersGoing: [], usersInterested: [], usersInvite: [], startTime: Date(), endTime: Date()))
+            }
+            eventIdStart += 1
+            invite = false
+            
+        }
+        for i in events {
+            createEvent(event: i) { [weak self] fuck in
+                guard let strongSelf = self else {
+                    return
+                }
+                switch fuck{
+                case .success(let f):
+                    print("succcccccccceeeeeeeedddd")
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+        
+    }
+    
+    public func testthequery(){
+        getAllPrivateEventsForMap(completion: { [weak self] result in
+            switch result {
+            case .success(let events):
+                print("sucess loading event: \(events)")
+
+                for event in events {
+                    print(event.eventId)
+                }
+                print("There are " + String(events.count) + " events you are invited too")
+            case .failure(let error):
+                print("Error loading events on map Error: \(error)")
+            }
+            print("done loading events")
+        })
+    }
+    
 }
 
 
