ce404c44453a558fa7c35775fee1fec0f87160e6
Gabe <gabe2002denton@gmail.com>
2022-09-09 14:13:58 -0500

    stupid file

---
+++ zip_official/zip_official/Frontend_Code/Objects/EventTypes/CategoryType.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryType.swift
+//  zip_official
+//
+//  Created by user on 9/9/22.
+//
+
+import Foundation
+
+public enum CategoryType: String {
+    case HappyHour = "Happy Hour"
+    case Trivia = "Trivia"
+    case Deal = "Deal"
+    case OpenMic = "Open Mic"
+    case Music = "Music"
+}
